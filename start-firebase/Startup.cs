4a100d4467555fb9d711674d940f9afc77154360
Алексей <burdykin@live.ru>
2017-07-15 14:13:43 +0300

    email авторизация

--- start-firebase/Startup.cs
+++ start-firebase/Startup.cs
@@ -7,16 +7,39 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+
+//
+
+
+
+
+
+
 
 namespace start_firebase
 {
     public class Startup
     {
+        //public Startup(IHostingEnvironment env)
+        //{
+        //    var builder = new ConfigurationBuilder()
+        //        .SetBasePath(env.ContentRootPath);
+        //        //.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        //        //.AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+        //        //.AddEnvironmentVariables();
+        //    Configuration = builder.Build();
+        //}
+        //public IConfigurationRoot Configuration { get; }
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+           
         }
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
@@ -28,10 +51,13 @@ namespace start_firebase
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+
+            app.UseStaticFiles(); //требуется установить пакет NuGet "Microsoft.AspNetCore.StaticFiles"
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
         }
     }
 }
---
+++ start-firebase/wwwroot/js/firebase-init.js
@@ -0,0 +1,10 @@
+﻿// Initialize Firebase (взять этот код из консоли firebase)
+var config = {
+    apiKey: "AIzaSyBXZXwmyVnV7tikF4UfFlYlP1kfJTZtwnw",
+    authDomain: "first-testing-bua.firebaseapp.com",
+    databaseURL: "https://first-testing-bua.firebaseio.com",
+    projectId: "first-testing-bua",
+    storageBucket: "first-testing-bua.appspot.com",
+    messagingSenderId: "264914602972"
+};
+firebase.initializeApp(config);
